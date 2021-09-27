78c379960a075c8909bed29f44e3aa40e50c3166
the-iconic-rihan <rihanbagwan5017@gmail.com>
2021-09-28 02:09:50 +0530

    second commit

--- NAEESHOP-GIT/main/main.js
+++ NAEESHOP-GIT/main/main.js
@@ -1,5 +1,4 @@
 let navLinks = document.getElementById("nav-links");
-let removeImgClass = document.getElementsByClassName("img");
 function hiddenMenu() {
     navLinks.style.right = "-200px";
 }
@@ -14,8 +13,8 @@ let subscribeBtn = document.getElementById("subscribeNow");
 function ChangeEmailPosition() {
     subscribeBtn.style.display = "block";
     subscribeBtn.style.marginLeft = "-2rem";
-    emailInput.style.borderBottomLeftRadius= "0.1rem";
-    emailInput.style.borderTopRightRadius= "0.1rem";
+    emailInput.style.borderBottomLeftRadius = "0.1rem";
+    emailInput.style.borderTopRightRadius = "0.1rem";
     emailInput.style.transition = "linear 1s all";
 
 }
@@ -25,7 +24,3 @@ function ChangeSubcribeBtnPosition() {
     subscribeBtn.style.display = "none";
 }
 
-function removeClassImg(){
-    removeImgClass.classList.remove("mystyle");
-
-}
